f7e0ae715e4143ebb8773780a49932e83f881f3c
red <arihanan@pm.me>
2019-10-12 11:48:50 +0300

    keyboard input

--- Water Week/Assets/scripts/namescreen.cs
+++ Water Week/Assets/scripts/namescreen.cs
@@ -17,7 +17,8 @@ public class namescreen : MonoBehaviour
 	int inty;
 	float notim = 1;
 	bool onoff = true;
-	char[] keypadthing = {'.','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','*','*'};
+	char[] keypadthing = {'\b','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','\n'};
+	KeyCode[] keymapping = {KeyCode.Backspace,KeyCode.A,KeyCode.B,KeyCode.C,KeyCode.D,KeyCode.E,KeyCode.F,KeyCode.G,KeyCode.H,KeyCode.I,KeyCode.J,KeyCode.K,KeyCode.L,KeyCode.M,KeyCode.N,KeyCode.O,KeyCode.P,KeyCode.Q,KeyCode.R,KeyCode.S,KeyCode.T,KeyCode.U,KeyCode.V,KeyCode.W,KeyCode.X,KeyCode.Y,KeyCode.Z,KeyCode.Space,KeyCode.Return};
 
 	void Start()
 	{
@@ -34,63 +35,90 @@ public class namescreen : MonoBehaviour
 
 	void Update()
 	{
+		bool keyboard = false;
 		int input = -1;
+
+		//mouse input
 		if(mouseinput.m.checkreigon(.2f,.2f,.8f,.7f))
 			input = Mathf.Min((int)((mouseinput.m.mp.x - .2f)*10) + ((int)((.7f - mouseinput.m.mp.y)*10))*6, 28);
+		//keyboard input
+		if((!Input.GetMouseButton(0) && !Input.GetMouseButtonUp(0)) || input < 0)
+			for(int i = 0; i < keypadthing.Length; i++)
+			{
+				if(Input.GetKey(keymapping[i]))
+				{
+					input = i;
+					keyboard = true;
+					keysss[i].sprite = keypresses[2];
+				}
+				if(Input.GetKeyUp(keymapping[i]))
+				{
+					handlechar(keypadthing[i]);
+					keysss[i].sprite = keypresses[0];
+				}
+			}
 		if(input != inty)
 		{
 			if(inty  >= 0) keysss[inty ].sprite = keypresses[0];
-			if(input >= 0) keysss[input].sprite = keypresses[1];
+			if(input >= 0 && !keyboard) keysss[input].sprite = keypresses[1];
 			inty = input;
 		}
 		if(Input.GetMouseButton(0) && input >= 0) keysss[input].sprite = keypresses[2];
 		if(Input.GetMouseButtonUp(0) && input >= 0)
 		{
 			keysss[input].sprite = keypresses[1];
-			switch(keypadthing[input])
-			{
-				case '.':
-					if(txtt.text == "")
-					{
-						if(notim >= 1) notim = 0;
-					}
-					else txtt.text = txtt.text.Substring(0, txtt.text.Length - 1);
-					break;
-				case '*':
-					if(txtt.text.Length > 0)
-					{
-						if(txtt.text.EndsWith(" ")) txtt.text = txtt.text.Substring(0,txtt.text.Length - 1);
-						onoff = false;
-						finaltext.text = txtt.text.ToUpper() + "'s bullet";
-					}
-					break;
-				case ' ':
-					if(txtt.text.Length >= 11)
-					{
-						if(notim >= 1) notim = 0;
-					}
-					else if(txtt.text != "" && !txtt.text.EndsWith(" ")) txtt.text += " ";
-					break;
-				default:
-					if(txtt.text.Length >= 11)
-					{
-						if(notim >= 1) notim = 0;
-					}
-					else txtt.text += keypadthing[input];
-					break;
-			}
-		}
+			handlechar(keypadthing[input]);
+		};
 
+		//animations
 		if(tim == 0) transform.localPosition = Vector2.zero;
 		tim = Mathf.Clamp(tim + Time.deltaTime * (onoff ? 1 : -1), 0, 1.5f);
 		notim += Time.deltaTime*1.5f;
 		for(int i = 0; i < keysss.Length; i++)
+		{
 			keysss[i].transform.localPosition = initpos[i+1] + new Vector2(0, keynimation.Evaluate(tim*1.2f - i*.1f + (i - i%6)*.08f)*140 - ((input == i && Input.GetMouseButton(0)) ? 1 : 0));
+		}
 		field.localPosition = initpos[0] + new Vector2(nono.Evaluate(notim)*5, (1 - fieldanim.Evaluate((tim - .3f)*1.1f))*60);
 		if(tim == 0 && !onoff)
 		{
 			actualgame.SetActive(true);
 			gameObject.SetActive(false);
+		}
+	}
+
+	void handlechar(char ch)
+	{
+		switch(ch)
+		{
+			case '\b':
+				if(txtt.text == "")
+				{
+					if(notim >= 1) notim = 0;
+				}
+				else txtt.text = txtt.text.Substring(0, txtt.text.Length - 1);
+				break;
+			case '\n':
+				if(txtt.text.Length > 0)
+				{
+					if(txtt.text.EndsWith(" ")) txtt.text = txtt.text.Substring(0,txtt.text.Length - 1);
+					onoff = false;
+					finaltext.text = txtt.text.ToUpper() + "'s bullet";
+				}
+				break;
+			case ' ':
+				if(txtt.text.Length >= 11)
+				{
+					if(notim >= 1) notim = 0;
+				}
+				else if(txtt.text != "" && !txtt.text.EndsWith(" ")) txtt.text += " ";
+				break;
+			default:
+				if(txtt.text.Length >= 11)
+				{
+					if(notim >= 1) notim = 0;
+				}
+				else txtt.text += ch;
+				break;
 		}
 	}
 }
