a464fbbc9c45bdb739528229262a5301fb311fd4
red <arihanan@pm.me>
2019-11-25 18:13:02 +0200

    computor ded

---
+++ Water Week/Assets/others/croncle.shader
@@ -0,0 +1,54 @@
+﻿Shader "Unlit/Croncle"
+{
+    Properties
+    {
+        _Col ("Color", Color) = (1,0,0,1)
+    }
+    SubShader
+    {
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Tags { "RenderType"="Transparent" "Queue"="Transparent" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv - 0.5;
+                return o;
+            }
+
+            fixed4 _Col;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = _Col;
+                float dist = sqrt(dot(i.uv,i.uv));
+                clip(saturate(0.5 - dist) - .01);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Water Week/Assets/shaders/ppl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ppl : MonoBehaviour
+{
+	[SerializeField] GameObject prsn;
+	[SerializeField] GameObject prsn2;
+	[SerializeField] GameObject treemaster;
+	[SerializeField] Sprite[] treesss;
+	[SerializeField] Image[] treethingies;
+	List<GameObject> pppp = new List<GameObject>();
+	const float min = 1;
+	const float max = 2;
+	const float width = 5;
+	int lastsecond;
+
+	void Start()
+	{
+		float read = -width - Random.Range(0,max);
+		while(read < width)
+		{
+			float much = Random.Range(min,max);
+			if(read < 0 && read + much > 0)
+				pppp.Add(Instantiate(prsn2, new Vector3(read,-2.88f,-5), Quaternion.Euler(0,90,0), transform));
+			else
+				pppp.Add(Instantiate(prsn, new Vector3(read,-2.88f,-5), Quaternion.Euler(0,90,0), transform));
+			pppp[pppp.Count - 1].GetComponent<Animator>().speed = Random.Range(.8f, 1.2f);
+			read += much;
+		}
+		foreach(Image img in treethingies)
+		{
+			img.transform.localPosition = new Vector3(-165 - Random.Range(-565,165),-1,70);
+			img.sprite = treesss[Random.Range(0,treesss.Length)];
+			img.SetNativeSize();
+		}
+	}
+
+	void Update()
+	{
+		foreach(Image img in treethingies)
+		{
+			if(img.transform.localPosition.x > 165)
+			{
+				img.transform.localPosition = new Vector3(-165 - Random.Range(0,400),-1,70);
+				img.sprite = treesss[Random.Range(0,treesss.Length)];
+				img.SetNativeSize();
+			}
+			else img.transform.Translate(Time.deltaTime*1,0,0,Space.Self);
+		}
+		if(Mathf.Floor(Time.time) != lastsecond)
+		{
+			lastsecond = Mathf.FloorToInt(Time.time);
+			foreach(Image img in treethingies)
+				img.transform.localPosition = new Vector3(Mathf.Round(img.transform.localPosition.x),-1,70);
+		}
+	}
+}
